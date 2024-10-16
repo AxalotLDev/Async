1ae3af391d8fd7d0893503b9e7a0a951421ea5e8
AxalotL <ladyginnic815@gmail.com>
2024-10-16 18:42:21 +0500

    Fix launch issue with lithium mod

--- src/main/java/com/axalotl/async/mixin/ServerWorldMixin.java
+++ src/main/java/com/axalotl/async/mixin/ServerWorldMixin.java
@@ -66,8 +66,6 @@ public abstract class ServerWorldMixin implements StructureWorldAccess {
     @Redirect(method = "<init>", at = @At(value = "NEW", target = "(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/world/level/storage/LevelStorage$Session;Lcom/mojang/datafixers/DataFixer;Lnet/minecraft/structure/StructureTemplateManager;Ljava/util/concurrent/Executor;Lnet/minecraft/world/gen/chunk/ChunkGenerator;IIZLnet/minecraft/server/WorldGenerationProgressListener;Lnet/minecraft/world/chunk/ChunkStatusChangeListener;Ljava/util/function/Supplier;)Lnet/minecraft/server/world/ServerChunkManager;"))
     private ServerChunkManager overwriteServerChunkManager(ServerWorld world, LevelStorage.Session session, DataFixer dataFixer, StructureTemplateManager structureTemplateManager, Executor workerExecutor, ChunkGenerator chunkGenerator, int viewDistance, int simulationDistance, boolean dsync, WorldGenerationProgressListener worldGenerationProgressListener, ChunkStatusChangeListener chunkStatusChangeListener, Supplier persistentStateManagerFactory) {
         if (FabricLoader.getInstance().isModLoaded("c2me")) {
-            return new ParaServerChunkProvider(world, session, dataFixer, structureTemplateManager, workerExecutor, chunkGenerator, viewDistance, simulationDistance, dsync, worldGenerationProgressListener, chunkStatusChangeListener, persistentStateManagerFactory);
-        } else {
             return new ServerChunkManager(
                     this.toServerWorld(),
                     session,
@@ -82,6 +80,8 @@ public abstract class ServerWorldMixin implements StructureWorldAccess {
                     this.entityManager::updateTrackingStatus,
                     () -> server.getOverworld().getPersistentStateManager()
             );
+        } else {
+            return new ParaServerChunkProvider(world, session, dataFixer, structureTemplateManager, workerExecutor, chunkGenerator, viewDistance, simulationDistance, dsync, worldGenerationProgressListener, chunkStatusChangeListener, persistentStateManagerFactory);
         }
     }
 
