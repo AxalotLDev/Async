bc24c1eec0c516b5d4ef80e70344162ab0692e2d
AxalotLDev <ladyginnic815@gmail.com>
2025-03-01 16:58:32 +0500

    fix: BreedTask

--- src/main/java/com/axalotl/async/mixin/entity/BreedTaskMixin.java
+++ src/main/java/com/axalotl/async/mixin/entity/BreedTaskMixin.java
@@ -1,24 +1,45 @@
 package com.axalotl.async.mixin.entity;
 
-import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import net.minecraft.entity.ai.brain.MemoryModuleType;
 import net.minecraft.entity.ai.brain.task.BreedTask;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.concurrent.locks.ReentrantLock;
 
 @Mixin(BreedTask.class)
-public class BreedTaskMixin {
+public abstract class BreedTaskMixin {
+    @Shadow
+    protected abstract AnimalEntity getBreedTarget(AnimalEntity animal);
+
     @Unique
     private static final ReentrantLock lock = new ReentrantLock();
 
-    @WrapMethod(method = "run(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/passive/AnimalEntity;J)V")
-    private void run(ServerWorld serverWorld, AnimalEntity animalEntity, long l, Operation<Void> original) {
+    @Inject(method = "shouldKeepRunning(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/passive/AnimalEntity;J)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/ai/brain/task/BreedTask;getBreedTarget(Lnet/minecraft/entity/passive/AnimalEntity;)Lnet/minecraft/entity/passive/AnimalEntity;"))
+    private void shouldKeepRunning(ServerWorld serverWorld, AnimalEntity animalEntity, long l, CallbackInfoReturnable<Boolean> cir) {
+        if (this.getBreedTarget(animalEntity) == null) {
+            cir.cancel();
+        }
+    }
+
+    @Inject(method = "keepRunning(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/passive/AnimalEntity;J)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/ai/brain/task/BreedTask;getBreedTarget(Lnet/minecraft/entity/passive/AnimalEntity;)Lnet/minecraft/entity/passive/AnimalEntity;"), cancellable = true)
+    private void canStillUse(ServerWorld serverWorld, AnimalEntity animalEntity, long l, CallbackInfo ci) {
+        if (this.getBreedTarget(animalEntity) == null) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "getBreedTarget", at = @At("HEAD"), cancellable = true)
+    private void syncBreedTarget(AnimalEntity animal, CallbackInfoReturnable<AnimalEntity> cir) {
         synchronized (lock) {
-            original.call(serverWorld, animalEntity, l);
+            cir.setReturnValue((AnimalEntity) animal.getBrain().getOptionalRegisteredMemory(MemoryModuleType.BREED_TARGET).orElse(null));
         }
     }
 }
