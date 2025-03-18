6e1aee7045d83936d37d37a127f6f39ad9985de4
AxalotLDev <ladyginnic815@gmail.com>
2025-03-18 11:34:57 +0500

    fix: timeout

--- src/main/java/com/axalotl/async/ParallelProcessor.java
+++ src/main/java/com/axalotl/async/ParallelProcessor.java
@@ -165,12 +165,12 @@ public class ParallelProcessor {
                 CompletableFuture<?> allTasks = CompletableFuture.allOf(
                         futuresList.toArray(new CompletableFuture[0])
                 );
-                allTasks.whenComplete((result, ex) -> {
-                    if (ex != null) {
-                        LOGGER.error("Timeout during entity tick processing", ex);
-                        watchdog();
-                        server.shutdown();
-                    }
+
+                allTasks.orTimeout(120, TimeUnit.SECONDS).exceptionally(ex -> {
+                    watchdog();
+                    LOGGER.error("Timeout during entity tick processing", ex);
+                    server.shutdown();
+                    return null;
                 });
 
                 server.getWorlds().forEach(world -> {
