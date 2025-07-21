98aa31dcfa2308d4606d21e1499fb182af299878
AxalotLDev <ladyginnic815@gmail.com>
2025-07-21 23:48:59 +0500

    fix: fix FoxBreed

--- common/src/main/java/com/axalotl/async/common/mixin/entity/FoxBreedGoalMixin.java
+++ common/src/main/java/com/axalotl/async/common/mixin/entity/FoxBreedGoalMixin.java
@@ -1,7 +1,6 @@
 package com.axalotl.async.common.mixin.entity;
 
 import net.minecraft.world.entity.ai.goal.BreedGoal;
-import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.animal.Fox;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -9,22 +8,47 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Mixin(Fox.FoxBreedGoal.class)
-public class FoxBreedGoalMixin extends BreedGoal {
+public abstract class FoxBreedGoalMixin extends BreedGoal {
 
-    public FoxBreedGoalMixin(Animal animal, double speedModifier) {
-        super(animal, speedModifier);
+    public FoxBreedGoalMixin(Fox fox, double speedModifier) {
+        super(fox, speedModifier);
     }
 
     @Unique
-    private final AtomicBoolean async$hasBred = new AtomicBoolean(false);
+    private static final ConcurrentHashMap<String, Boolean> async$breedingPairs = new ConcurrentHashMap<>();
+
+    @Inject(method = "start", at = @At("HEAD"))
+    private void resetBreedingFlag(CallbackInfo ci) {
+        async$breedingPairs.remove(async$getPairKey());
+    }
 
     @Inject(method = "breed", at = @At("HEAD"), cancellable = true)
-    private void asyncSafeBreed(CallbackInfo ci) {
-        if (!async$hasBred.compareAndSet(false, true)) {
+    private void preventDoubleBreed(CallbackInfo ci) {
+        String pairKey = async$getPairKey();
+        if (async$breedingPairs.putIfAbsent(pairKey, Boolean.TRUE) != null) {
             ci.cancel();
         }
+    }
+
+    @Unique
+    private String async$getPairKey() {
+        UUID id1 = this.animal.getUUID();
+        UUID id2 = null;
+        if (this.partner != null) {
+            id2 = this.partner.getUUID();
+        }
+        String s1 = id1.toString();
+        String s2 = null;
+        if (id2 != null) {
+            s2 = id2.toString();
+        }
+        if (s2 != null) {
+            return s1.compareTo(s2) <= 0 ? s1 + "|" + s2 : s2 + "|" + s1;
+        }
+        return s1;
     }
 }
