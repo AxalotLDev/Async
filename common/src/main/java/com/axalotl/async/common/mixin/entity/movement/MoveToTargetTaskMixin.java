67c13a2764c782402d6459f14b1239fb88cdbbef
AxalotLDev <ladyginnic815@gmail.com>
2025-07-23 23:15:21 +0500

    fix: fix move to target

--- common/src/main/java/com/axalotl/async/common/mixin/entity/movement/MoveToTargetTaskMixin.java
+++ common/src/main/java/com/axalotl/async/common/mixin/entity/movement/MoveToTargetTaskMixin.java
@@ -2,7 +2,6 @@ package com.axalotl.async.common.mixin.entity.movement;
 
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.ai.Brain;
 import net.minecraft.world.entity.ai.behavior.MoveToTargetSink;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.ai.memory.WalkTarget;
@@ -16,10 +15,9 @@ import java.util.Optional;
 @Mixin(MoveToTargetSink.class)
 public class MoveToTargetTaskMixin {
 
-    @Inject(method = "checkExtraStartConditions(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/entity/Mob;)Z", at = @At("HEAD"), cancellable = true)
-    private void cancelIfNoWalkTarget(ServerLevel level, Mob owner, CallbackInfoReturnable<Boolean> cir) {
-        Brain<?> brain = owner.getBrain();
-        Optional<WalkTarget> optional = brain.getMemory(MemoryModuleType.WALK_TARGET);
+    @Inject(method = "canStillUse(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/entity/Mob;J)Z", at = @At("HEAD"), cancellable = true)
+    private void skipIfNoWalkTarget(ServerLevel level, Mob entity, long gameTime, CallbackInfoReturnable<Boolean> cir) {
+        Optional<WalkTarget> optional = entity.getBrain().getMemory(MemoryModuleType.WALK_TARGET);
         if (optional.isEmpty()) {
             cir.setReturnValue(false);
         }
