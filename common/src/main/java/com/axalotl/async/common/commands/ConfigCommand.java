7c8f294cf41d13321641a1fa86ad2ccd3615b05c
AxalotLDev <ladyginnic815@gmail.com>
2025-07-29 12:27:43 +0500

    update: port from dev && fixes

--- common/src/main/java/com/axalotl/async/common/commands/ConfigCommand.java
+++ common/src/main/java/com/axalotl/async/common/commands/ConfigCommand.java
@@ -23,17 +23,17 @@ public class ConfigCommand {
     public static LiteralArgumentBuilder<CommandSourceStack> registerConfig(LiteralArgumentBuilder<CommandSourceStack> root) {
         return root.then(literal("config")
                 .then(literal("toggle").requires(cmdSrc -> cmdSrc.hasPermission(4)).executes(cmdCtx -> {
-                    AsyncConfig.disabled.setValue(!AsyncConfig.disabled.getValue());
+                    AsyncConfig.disabled = !AsyncConfig.disabled;
                     saveConfig();
                     MutableComponent message = prefix.copy().append(Component.literal("Async is now ").withStyle(style -> style.withColor(ChatFormatting.WHITE)))
-                            .append(Component.literal(AsyncConfig.disabled.getValue() ? "disabled" : "enabled").withStyle(style -> style.withColor(ChatFormatting.GREEN)));
+                            .append(Component.literal(AsyncConfig.disabled ? "disabled" : "enabled").withStyle(style -> style.withColor(ChatFormatting.GREEN)));
                     cmdCtx.getSource().sendSuccess(() -> message, true);
                     return 1;
                 }))
                 .then(literal("synchronizedEntities")
                         .requires(cmdSrc -> cmdSrc.hasPermission(4))
                         .executes(cmdCtx -> {
-                            Set<ResourceLocation> currentValue = AsyncConfig.synchronizedEntities.getValue();
+                            Set<ResourceLocation> currentValue = AsyncConfig.synchronizedEntities;
                             MutableComponent message = prefix.copy().append(Component.literal("Synchronized Entities: ").withStyle(style -> style.withColor(ChatFormatting.WHITE)));
                             if (currentValue.isEmpty()) {
                                 message.append(Component.literal("No entities synchronized.").withStyle(style -> style.withColor(ChatFormatting.RED)));
@@ -51,7 +51,7 @@ public class ConfigCommand {
                         .then(literal("add")
                                 .then(Commands.argument("entity", ResourceLocationArgument.id()).suggests(SuggestionProviders.cast(SuggestionProviders.SUMMONABLE_ENTITIES)).executes(cmdCtx -> {
                                     ResourceLocation id = ResourceLocationArgument.getId(cmdCtx, "entity");
-                                    if (AsyncConfig.synchronizedEntities.getValue().contains(id)) {
+                                    if (AsyncConfig.synchronizedEntities.contains(id)) {
                                         MutableComponent message = prefix.copy()
                                                 .append(Component.literal("Error entity class ").withStyle(style -> style.withColor(ChatFormatting.RED)))
                                                 .append(Component.literal(id.toString()).withStyle(style -> style.withColor(ChatFormatting.RED)))
@@ -70,12 +70,12 @@ public class ConfigCommand {
                         .then(literal("remove")
                                 .then(Commands.argument("entity", ResourceLocationArgument.id())
                                         .suggests((context, builder) -> {
-                                            AsyncConfig.synchronizedEntities.getValue().forEach(id -> builder.suggest(id.toString()));
+                                            AsyncConfig.synchronizedEntities.forEach(id -> builder.suggest(id.toString()));
                                             return builder.buildFuture();
                                         })
                                         .executes(cmdCtx -> {
                                             ResourceLocation identifier = cmdCtx.getArgument("entity", ResourceLocation.class);
-                                            if (!AsyncConfig.synchronizedEntities.getValue().contains(identifier)) {
+                                            if (!AsyncConfig.synchronizedEntities.contains(identifier)) {
                                                 MutableComponent message = prefix.copy()
                                                         .append(Component.literal("Error entity class ").withStyle(style -> style.withColor(ChatFormatting.RED)))
                                                         .append(Component.literal(identifier.toString()).withStyle(style -> style.withColor(ChatFormatting.RED)))
@@ -93,7 +93,7 @@ public class ConfigCommand {
                                         }))))
                 .then(literal("setAsyncEntitySpawn").requires(cmdSrc -> cmdSrc.hasPermission(4))
                         .executes(cmdCtx -> {
-                            boolean currentValue = AsyncConfig.enableAsyncSpawn.getValue();
+                            boolean currentValue = AsyncConfig.enableAsyncSpawn;
                             MutableComponent message = prefix.copy().append(Component.literal("Current value of async entity spawn: ").withStyle(style -> style.withColor(ChatFormatting.WHITE)))
                                     .append(Component.literal(String.valueOf(currentValue)).withStyle(style -> style.withColor(ChatFormatting.GREEN)));
                             cmdCtx.getSource().sendSuccess(() -> message, false);
@@ -101,7 +101,7 @@ public class ConfigCommand {
                         })
                         .then(Commands.argument("value", BoolArgumentType.bool()).executes(cmdCtx -> {
                             boolean value = BoolArgumentType.getBool(cmdCtx, "value");
-                            AsyncConfig.enableAsyncSpawn.setValue(value);
+                            AsyncConfig.enableAsyncSpawn = value;
                             PlatformEventBus.saveConfig();
                             MutableComponent message = prefix.copy().append(Component.literal("Async Entity Spawn set to ").withStyle(style -> style.withColor(ChatFormatting.WHITE)))
                                     .append(Component.literal(String.valueOf(value)).withStyle(style -> style.withColor(ChatFormatting.GREEN)));
