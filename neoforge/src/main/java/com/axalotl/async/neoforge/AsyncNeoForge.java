45cb680738dca8c7379cbb125dd45c427a23c5f9
AxalotLDev <ladyginnic815@gmail.com>
2025-07-29 21:24:41 +0500

    fix: remove lithium dependency

--- neoforge/src/main/java/com/axalotl/async/neoforge/AsyncNeoForge.java
+++ neoforge/src/main/java/com/axalotl/async/neoforge/AsyncNeoForge.java
@@ -8,7 +8,6 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.logging.LogUtils;
 import net.minecraft.commands.CommandSourceStack;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.javafmlmod.FMLModContainer;
@@ -26,13 +25,11 @@ import static com.axalotl.async.neoforge.config.AsyncConfig.loadConfig;
 public class AsyncNeoForge {
     public static final String MOD_ID = "async";
     public static final Logger LOGGER = LogUtils.getLogger();
-    public static boolean LITHIUM = ModList.get().isLoaded("lithium");
-    public static boolean VMP = ModList.get().isLoaded("vmp");
 
     public AsyncNeoForge(FMLModContainer container) {
         LOGGER.info("Initializing Async...");
-        NeoForge.EVENT_BUS.register(this);
         NeoForgePlatformEvents.init();
+        NeoForge.EVENT_BUS.register(this);
         LOGGER.info("Initializing Async Config...");
         container.registerConfig(ModConfig.Type.COMMON, SPEC, "async.toml");
         LOGGER.info("Async Initialized Successfully!");
