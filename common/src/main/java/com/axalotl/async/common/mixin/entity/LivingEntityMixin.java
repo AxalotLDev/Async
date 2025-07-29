54942b9b10ee81b0f500245b954c94fc2479a661
AxalotLDev <ladyginnic815@gmail.com>
2025-07-29 19:35:52 +0500

    fix: fix remove status effect

--- common/src/main/java/com/axalotl/async/common/mixin/entity/LivingEntityMixin.java
+++ common/src/main/java/com/axalotl/async/common/mixin/entity/LivingEntityMixin.java
@@ -17,6 +17,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Map;
@@ -65,6 +66,13 @@ public abstract class LivingEntityMixin extends Entity {
             return original.call(instance, entity, runnable);
         } else {
             return false;
+        }
+    }
+
+    @Inject(method = "onEffectRemoved", at = @At("HEAD"), cancellable = true)
+    private void async$onEffectRemoved(MobEffectInstance effectInstance, CallbackInfo ci) {
+        if (effectInstance == null) {
+            ci.cancel();
         }
     }
 
