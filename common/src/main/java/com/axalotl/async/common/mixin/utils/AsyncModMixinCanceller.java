7344e12e32c0eb68611c4e2f846052351ffc82ec
AxalotLDev <ladyginnic815@gmail.com>
2025-07-30 11:29:25 +0500

    update: port from 1.21.1

--- common/src/main/java/com/axalotl/async/common/mixin/utils/AsyncModMixinCanceller.java
+++ common/src/main/java/com/axalotl/async/common/mixin/utils/AsyncModMixinCanceller.java
@@ -6,17 +6,28 @@ import java.util.List;
 
 public class AsyncModMixinCanceller implements MixinCanceller {
     private boolean LITHIUM = false;
+    private boolean VMP = false;
 
     @Override
     public boolean shouldCancel(List<String> targetClassNames, String mixinClassName) {
         if (mixinClassName.contains("lithium") && !mixinClassName.contains("async")) {
             LITHIUM = true;
         }
+        if (mixinClassName.contains("vmp") && !mixinClassName.contains("async")) {
+            VMP = true;
+        }
+        switch (mixinClassName) {
+            case "com.ishland.c2me.fixes.general.threading_issues.mixin.asynccatchers.MixinThreadedAnvilChunkStorage":
+            case "com.ishland.c2me.fixes.worldgen.threading_issues.mixin.threading_detections.random_instances.MixinWorld":
+                return true;
+        }
         if (mixinClassName.endsWith("com.axalotl.async.common.mixin.lithium.LithiumServerChunkCacheMixin") ||
                 mixinClassName.endsWith("com.axalotl.async.common.mixin.lithium.LithiumServerLevel")) {
             return !LITHIUM;
         }
-        return mixinClassName.equals("com.ishland.c2me.fixes.general.threading_issues.mixin.asynccatchers.MixinThreadedAnvilChunkStorage") ||
-                mixinClassName.equals("com.ishland.c2me.fixes.worldgen.threading_issues.mixin.threading_detections.random_instances.MixinWorld");
+        if (mixinClassName.endsWith("com.axalotl.async.common.mixin.vmp.VMPChunkMapMixin")) {
+            return !VMP;
+        }
+        return false;
     }
 }
