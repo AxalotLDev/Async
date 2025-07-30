f3052552ed9bb0255a1d547e9809e93142908346
AxalotLDev <ladyginnic815@gmail.com>
2025-07-30 11:30:32 +0500

    update: port from 1.21.1

--- common/src/main/java/com/axalotl/async/common/AsyncCommon.java
+++ common/src/main/java/com/axalotl/async/common/AsyncCommon.java
@@ -4,5 +4,4 @@ import static com.axalotl.async.common.platform.PlatformEventBus.isModLoaded;
 
 public class AsyncCommon {
     public static boolean LITHIUM = isModLoaded("lithium");
-    public static String platform = "neoforge";
 }
