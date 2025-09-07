891dcb6a8a723487d86ee0b495b0f1a940a8fbdc
AxalotLDev <ladyginnic815@gmail.com>
2025-09-07 21:30:42 +0500

    fix: fix final warning

--- common/src/main/java/com/axalotl/async/common/mixin/entity/AttributeInstanceMixin.java
+++ common/src/main/java/com/axalotl/async/common/mixin/entity/AttributeInstanceMixin.java
@@ -4,7 +4,6 @@ import com.axalotl.async.common.parallelised.ConcurrentCollections;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
@@ -14,14 +13,11 @@ import java.util.Map;
 public class AttributeInstanceMixin {
 
     @Shadow
-    @Final
-    private Map<AttributeModifier.Operation, Map<ResourceLocation, AttributeModifier>> modifiersByOperation = ConcurrentCollections.newHashMap();
+    private final Map<AttributeModifier.Operation, Map<ResourceLocation, AttributeModifier>> modifiersByOperation = ConcurrentCollections.newHashMap();
 
     @Shadow
-    @Final
-    private Map<ResourceLocation, AttributeModifier> modifierById = ConcurrentCollections.newHashMap();
+    private final Map<ResourceLocation, AttributeModifier> modifierById = ConcurrentCollections.newHashMap();
 
     @Shadow
-    @Final
-    private Map<ResourceLocation, AttributeModifier> permanentModifiers = ConcurrentCollections.newHashMap();
+    private final Map<ResourceLocation, AttributeModifier> permanentModifiers = ConcurrentCollections.newHashMap();
 }
