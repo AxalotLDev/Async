97948588c62141059fe877804e35123a9645356f
AxalotLDev <ladyginnic815@gmail.com>
2025-09-07 21:58:06 +0500

    fix: fix EntityTickList 2

--- common/src/main/java/com/axalotl/async/common/mixin/entity/EntityTickListMixin.java
+++ common/src/main/java/com/axalotl/async/common/mixin/entity/EntityTickListMixin.java
@@ -4,16 +4,25 @@ import com.axalotl.async.common.parallelised.fastutil.Int2ObjectConcurrentHashMa
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import it.unimi.dsi.fastutil.objects.ObjectCollection;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.entity.EntityTickList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
 
 @Mixin(EntityTickList.class)
 public class EntityTickListMixin {
-    @Shadow private Int2ObjectMap<Entity> passive = new Int2ObjectConcurrentHashMap<>();
-    @Shadow private Int2ObjectMap<Entity> active = new Int2ObjectConcurrentHashMap<>();
+    @Shadow
+    private Int2ObjectMap<Entity> passive = new Int2ObjectConcurrentHashMap<>();
+    @Shadow
+    private Int2ObjectMap<Entity> active;
+    @Shadow
+    private Int2ObjectMap<Entity> iterated;
 
     @Unique
     private final Object async$lock = new Object();
@@ -30,5 +39,16 @@ public class EntityTickListMixin {
         synchronized (async$lock) {
             original.call(entity);
         }
+    }
+
+    @Redirect(
+            method = "forEach",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lit/unimi/dsi/fastutil/ints/Int2ObjectMap;values()Lit/unimi/dsi/fastutil/objects/ObjectCollection;"
+            )
+    )
+    private ObjectCollection<Entity> redirectForEachValues(Int2ObjectMap<Entity> instance) {
+        return new ObjectArrayList<>(instance.values());
     }
 }
