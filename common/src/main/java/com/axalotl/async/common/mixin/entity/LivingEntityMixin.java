2f825e4e0aca22ea174ec1241702033bdc1a041f
AxalotLDev <ladyginnic815@gmail.com>
2025-10-12 11:19:02 +0500

    fix: fix get block state

--- common/src/main/java/com/axalotl/async/common/mixin/entity/LivingEntityMixin.java
+++ common/src/main/java/com/axalotl/async/common/mixin/entity/LivingEntityMixin.java
@@ -12,13 +12,10 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Collections;
 import java.util.List;
@@ -110,11 +107,5 @@ public abstract class LivingEntityMixin extends Entity {
         synchronized (async$lock) {
             return original.call();
         }
-    }
-
-    @Inject(method = "onClimbable", at = @At("HEAD"), cancellable = true)
-    private void isClimbing(CallbackInfoReturnable<Boolean> cir) {
-        BlockState blockState = this.getInBlockState();
-        if (blockState == null) cir.setReturnValue(false);
     }
 }
