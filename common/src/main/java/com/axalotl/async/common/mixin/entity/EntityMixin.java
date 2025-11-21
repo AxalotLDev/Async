4fe3353b70c8019203787280b7ee3c1f7c7e74bd
AxalotLDev <ladyginnic815@gmail.com>
2025-11-21 11:13:17 +0500

    update: fix && update dependencies - fix deadlocks with lithium on neoforge - update dependencies - port changes from 1.21.10

--- common/src/main/java/com/axalotl/async/common/mixin/entity/EntityMixin.java
+++ common/src/main/java/com/axalotl/async/common/mixin/entity/EntityMixin.java
@@ -2,27 +2,14 @@ package com.axalotl.async.common.mixin.entity;
 
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.chunk.ChunkAccess;
-import net.minecraft.world.level.chunk.LevelChunk;
-import net.minecraft.world.level.chunk.status.ChunkStatus;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
-
-    @Shadow
-    public abstract Level level();
-
-    @Shadow
-    public abstract BlockPos blockPosition();
 
     @Unique
     private static final Object async$lock = new Object();
@@ -37,30 +24,8 @@ public abstract class EntityMixin {
     @WrapMethod(method = "getInBlockState")
     private BlockState wrapGetInBlockState(Operation<BlockState> original) {
         BlockState blockState = original.call();
-
-        if (blockState == null && this.level() instanceof ServerLevel serverLevel) {
-            BlockPos pos = this.blockPosition();
-
-            LevelChunk chunk = serverLevel.getChunkSource()
-                    .getChunkNow(pos.getX() >> 4, pos.getZ() >> 4);
-
-            if (chunk != null) {
-                return chunk.getBlockState(pos);
-            }
-
-            ChunkAccess access = serverLevel.getChunkSource()
-                    .getChunk(pos.getX() >> 4, pos.getZ() >> 4, ChunkStatus.FULL, true);
-
-            if (access instanceof LevelChunk levelChunk) {
-                return levelChunk.getBlockState(pos);
-            }
-
-            return Blocks.AIR.defaultBlockState();
-        }
-
-        return blockState;
+        return blockState != null ? blockState : Blocks.AIR.defaultBlockState();
     }
-
 
     @WrapMethod(method = "addPassenger")
     private void addPassenger(Entity passenger, Operation<Void> original) {
