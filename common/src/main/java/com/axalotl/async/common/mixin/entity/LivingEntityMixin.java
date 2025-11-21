74a6c63a6956375a0605494f22e7079589916a78
AxalotLDev <ladyginnic815@gmail.com>
2025-11-21 19:52:15 +0500

    fix: fix && refactor effects

--- common/src/main/java/com/axalotl/async/common/mixin/entity/LivingEntityMixin.java
+++ common/src/main/java/com/axalotl/async/common/mixin/entity/LivingEntityMixin.java
@@ -3,7 +3,10 @@ package com.axalotl.async.common.mixin.entity;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -11,12 +14,14 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -60,11 +65,7 @@ public abstract class LivingEntityMixin extends Entity {
 
     @WrapOperation(method = "tickEffects", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/effect/MobEffectInstance;tick(Lnet/minecraft/world/entity/LivingEntity;Ljava/lang/Runnable;)Z"))
     private boolean tickEffects(MobEffectInstance instance, LivingEntity entity, Runnable runnable, Operation<Boolean> original) {
-        if (instance != null) {
-            return original.call(instance, entity, runnable);
-        } else {
-            return false;
-        }
+        return instance != null ? original.call(instance, entity, runnable) : false;
     }
 
     @Inject(method = "onEffectRemoved", at = @At("HEAD"), cancellable = true)
@@ -77,21 +78,36 @@ public abstract class LivingEntityMixin extends Entity {
     @WrapMethod(method = "addEffect(Lnet/minecraft/world/effect/MobEffectInstance;Lnet/minecraft/world/entity/Entity;)Z")
     private boolean addEffect(MobEffectInstance effect, Entity source, Operation<Boolean> original) {
         synchronized (async$lock) {
-            return original.call(effect, source);
+            return effect != null ? original.call(effect, source) : false;
         }
     }
 
     @WrapMethod(method = "removeEffect")
     private boolean removeEffect(Holder<MobEffect> effect, Operation<Boolean> original) {
         synchronized (async$lock) {
-            return original.call(effect);
+            return effect != null ? original.call(effect) : false;
         }
+    }
+
+    @WrapMethod(method = "hasEffect")
+    public boolean hasEffect(Holder<MobEffect> effect, Operation<Boolean> original) {
+        return effect != null ? original.call(effect) : false;
     }
 
     @WrapMethod(method = "removeAllEffects")
     private boolean removeAllEffects(Operation<Boolean> original) {
         synchronized (async$lock) {
             return original.call();
+        }
+    }
+
+    @Inject(method = "causeFallDamage", at = @At("HEAD"), cancellable = true)
+    private void causeFallDamage(float fallDistance, float multiplier, DamageSource source, CallbackInfoReturnable<Boolean> cir) {
+        BlockPos pos = new BlockPos(Mth.floor(this.getX()), Mth.floor(this.getY()), Mth.floor(this.getZ()));
+        BlockState currentBlock = this.level().getBlockState(pos);
+
+        if (currentBlock.is(BlockTags.CLIMBABLE)) {
+            cir.setReturnValue(false);
         }
     }
 }
