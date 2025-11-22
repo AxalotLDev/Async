e033d0f197cd99710bb411c9ba8582e51a14884a
AxalotLDev <ladyginnic815@gmail.com>
2025-11-22 12:34:04 +0500

    fix: try fix brain memory

--- common/src/main/java/com/axalotl/async/common/mixin/entity/BrainMixin.java
+++ common/src/main/java/com/axalotl/async/common/mixin/entity/BrainMixin.java
@@ -1,13 +1,17 @@
 package com.axalotl.async.common.mixin.entity;
 
 import com.axalotl.async.common.parallelised.ConcurrentCollections;
-import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.world.entity.ai.Brain;
 import net.minecraft.world.entity.ai.memory.ExpirableValue;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Map;
 import java.util.Optional;
@@ -16,14 +20,20 @@ import java.util.Optional;
 public class BrainMixin {
 
     @Shadow
-    private final Map<MemoryModuleType<?>, Optional<? extends ExpirableValue<?>>> memories = ConcurrentCollections.newHashMap();
+    @Final
+    @Mutable
+    private Map<MemoryModuleType<?>, Optional<? extends ExpirableValue<?>>> memories;
 
-    @WrapMethod(method = "getMemory")
-    private <U> Optional<U> wrapGetMemory(MemoryModuleType<U> type, Operation<Optional<U>> original) {
-        Optional<U> result = original.call(type);
-        if (result == null || result.isEmpty()) {
-            return Optional.empty();
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void onInit(CallbackInfo ci) {
+        this.memories = ConcurrentCollections.newHashMap();
+    }
+
+    @Inject(method = "getMemory", at = @At("HEAD"), cancellable = true)
+    private <U> void cancelIfMemoryNullOrEmpty(MemoryModuleType<U> type, CallbackInfoReturnable<Optional<U>> cir) {
+        Optional<? extends ExpirableValue<?>> optional = this.memories.get(type);
+        if (optional.orElse(null) == null) {
+            cir.cancel();
         }
-        return result;
     }
 }
