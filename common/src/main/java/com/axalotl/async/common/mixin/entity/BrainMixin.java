8fb5313435c8f812b21994519b2b32a5f4db3171
AxalotLDev <ladyginnic815@gmail.com>
2025-11-22 18:13:04 +0500

    fix: fix brain memory

--- common/src/main/java/com/axalotl/async/common/mixin/entity/BrainMixin.java
+++ common/src/main/java/com/axalotl/async/common/mixin/entity/BrainMixin.java
@@ -6,13 +6,7 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.world.entity.ai.Brain;
 import net.minecraft.world.entity.ai.memory.ExpirableValue;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.*;
 
 import java.util.Map;
 import java.util.Optional;
@@ -21,21 +15,22 @@ import java.util.Optional;
 public class BrainMixin {
 
     @Shadow
-    @Final
-    @Mutable
-    private Map<MemoryModuleType<?>, Optional<? extends ExpirableValue<?>>> memories;
+    private final Map<MemoryModuleType<?>, Optional<? extends ExpirableValue<?>>> memories = ConcurrentCollections.newHashMap();
 
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void onInit(CallbackInfo ci) {
-        this.memories = ConcurrentCollections.newHashMap();
+    @Unique
+    private static final Object async$lock = new Object();
+
+    @WrapMethod(method = "setMemoryInternal")
+    private <U> void setMemoryInternal(MemoryModuleType<U> memoryType, Optional<? extends ExpirableValue<?>> memory, Operation<Void> original) {
+        synchronized (async$lock) {
+            original.call(memoryType, memory);
+        }
     }
 
-    @WrapMethod(method = "getMemory")
-    private <U> Optional<U> wrapGetMemory(MemoryModuleType<U> type, Operation<Optional<U>> original) {
-        Optional<U> result = original.call(type);
-        if (result == null || result.isEmpty()) {
-            return Optional.empty();
+    @WrapMethod(method = "clearMemories")
+    private void clearMemories(Operation<Void> original) {
+        synchronized (async$lock) {
+            original.call();
         }
-        return result;
     }
 }
