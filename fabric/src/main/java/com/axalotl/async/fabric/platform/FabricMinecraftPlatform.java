ef6e3b7c801dcbcabaf03dc6c2a10c9a48aabd27
AxalotLDev <ladyginnic815@gmail.com>
2025-12-10 00:34:37 +0500

    fix: temporal no permission api

--- fabric/src/main/java/com/axalotl/async/fabric/platform/FabricMinecraftPlatform.java
+++ fabric/src/main/java/com/axalotl/async/fabric/platform/FabricMinecraftPlatform.java
@@ -1,15 +1,14 @@
 package com.axalotl.async.fabric.platform;
 
-import com.axalotl.async.common.AsyncCommon;
 import com.axalotl.async.common.platform.MinecraftPlatform;
-import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.server.permissions.Permission;
+import net.minecraft.server.permissions.PermissionLevel;
 
 public class FabricMinecraftPlatform implements MinecraftPlatform {
 
     @Override
     public boolean hasPermission(CommandSourceStack source, String node, int level) {
-        String permission = String.format("%s.%s", AsyncCommon.MODID, node);
-        return Permissions.check(source, permission, level);
+        return source.permissions().hasPermission(new Permission.HasCommandLevel(PermissionLevel.byId(level)));
     }
 }
