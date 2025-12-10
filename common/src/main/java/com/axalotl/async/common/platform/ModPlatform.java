172d944668926f3ce4ae73f91394c53afe40d000
AxalotLDev <ladyginnic815@gmail.com>
2025-12-10 11:23:15 +0500

    update: update config command - Add reload command

--- common/src/main/java/com/axalotl/async/common/platform/ModPlatform.java
+++ common/src/main/java/com/axalotl/async/common/platform/ModPlatform.java
@@ -3,6 +3,8 @@ package com.axalotl.async.common.platform;
 public interface ModPlatform {
     void saveConfig();
 
+    void reloadConfig();
+
     boolean isModLoaded(String id);
 
     boolean platformUsesRefmap();
