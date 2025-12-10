90d0e40c4841703e6189eff77c3f54ae63f54045
AxalotLDev <ladyginnic815@gmail.com>
2025-12-10 12:31:45 +0500

    fix: fix platform initialize

--- fabric/src/main/java/com/axalotl/async/fabric/AsyncFabric.java
+++ fabric/src/main/java/com/axalotl/async/fabric/AsyncFabric.java
@@ -1,5 +1,6 @@
 package com.axalotl.async.fabric;
 
+import com.axalotl.async.common.AsyncCommon;
 import com.axalotl.async.common.ParallelProcessor;
 import com.axalotl.async.common.commands.AsyncCommand;
 import com.axalotl.async.common.commands.StatsCommand;
@@ -7,18 +8,16 @@ import com.axalotl.async.common.config.AsyncConfig;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-import net.fabricmc.loader.api.FabricLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class AsyncFabric implements ModInitializer {
+public class AsyncFabric extends AsyncCommon implements ModInitializer {
     public static final Logger LOGGER = LogManager.getLogger(AsyncFabric.class);
-    public static boolean LITHIUM = FabricLoader.getInstance().isModLoaded("lithium");
-    public static boolean VMP = FabricLoader.getInstance().isModLoaded("vmp");
 
     @Override
     public void onInitialize() {
         LOGGER.info("Initializing Async...");
+        this.initialize();
         com.axalotl.async.fabric.config.AsyncConfig.init();
 
         StatsCommand.runStatsThread();
