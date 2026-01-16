74247c6d6bd211752968d8f6116ad09f747b8183
furrymileon <gsafonov76@gmail.com>
2026-01-16 05:20:14 +0300

    Fix mob duplication during conversion

--- common/src/main/java/com/axalotl/async/common/mixin/entity/MobMixin.java
+++ common/src/main/java/com/axalotl/async/common/mixin/entity/MobMixin.java
@@ -3,15 +3,15 @@ package com.axalotl.async.common.mixin.entity;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
+import org.jspecify.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(Mob.class)
-public class MobMixin {
+public abstract class MobMixin {
 
     @Unique
     private static final Object async$lock = new Object();
@@ -38,9 +38,25 @@ public class MobMixin {
     }
 
     @WrapMethod(method = "setBodyArmorItem")
-    private void equipLootStack(ItemStack stack, Operation<Void> original) {
+    private void setBodyArmor(ItemStack stack, Operation<Void> original) {
         synchronized (async$lock) {
             original.call(stack);
+        }
+    }
+
+    @WrapMethod(method = "convertTo(Lnet/minecraft/world/entity/EntityType;Lnet/minecraft/world/entity/ConversionParams;Lnet/minecraft/world/entity/EntitySpawnReason;Lnet/minecraft/world/entity/ConversionParams$AfterConversion;)Lnet/minecraft/world/entity/Mob;")
+    private <T extends Mob> @Nullable T convertTo(
+            EntityType<T> entityType,
+            ConversionParams conversionParams,
+            EntitySpawnReason spawnReason,
+            ConversionParams.AfterConversion<T> afterConversion,
+            Operation<T> original
+    ) {
+        synchronized (async$lock) {
+            if (((Mob)(Object)this).isRemoved()) {
+                return null;
+            }
+            return original.call(entityType, conversionParams, spawnReason, afterConversion);
         }
     }
 }
