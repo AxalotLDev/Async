87c44b6a29d3108b8cbd2ba98dc1b4742097c553
AxalotLDev <ladyginnic815@gmail.com>
2026-02-04 12:05:42 +0500

    Fix non-blocking block destruction

--- common/src/main/java/com/axalotl/async/common/mixin/world/ChunkHolderMixin.java
+++ common/src/main/java/com/axalotl/async/common/mixin/world/ChunkHolderMixin.java
@@ -2,19 +2,18 @@ package com.axalotl.async.common.mixin.world;
 
 import com.axalotl.async.common.parallelised.fastutil.ConcurrentShortHashSet;
 import it.unimi.dsi.fastutil.shorts.ShortSet;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.SectionPos;
 import net.minecraft.server.level.ChunkHolder;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.LevelHeightAccessor;
+import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraft.world.level.lighting.LevelLightEngine;
-import org.objectweb.asm.Opcodes;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = ChunkHolder.class, priority = 1500)
 public abstract class ChunkHolderMixin {
@@ -24,13 +23,54 @@ public abstract class ChunkHolderMixin {
     @Final
     private ShortSet[] changedBlocksPerSection;
 
-    @Inject(method = "<init>", at = @At(value = "TAIL", target = "Lnet/minecraft/server/level/ChunkHolder;changedBlocksPerSection:[Lit/unimi/dsi/fastutil/shorts/ShortSet;", opcode = Opcodes.PUTFIELD))
-    private void overwriteShortSet(ChunkPos pos, int level, LevelHeightAccessor world, LevelLightEngine lightingProvider, ChunkHolder.LevelChangeListener levelUpdateListener, ChunkHolder.PlayerProvider playersWatchingChunkProvider, CallbackInfo ci) {
+    @Shadow
+    private boolean hasChangedSections;
+
+    @Shadow
+    @Final
+    private LevelHeightAccessor levelHeightAccessor;
+
+    @Shadow
+    public abstract LevelChunk getTickingChunk();
+
+    @Unique
+    private final Object async$lock = new Object();
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void initConcurrentSets(
+            ChunkPos pos,
+            int level,
+            LevelHeightAccessor world,
+            LevelLightEngine lightingProvider,
+            ChunkHolder.LevelChangeListener levelUpdateListener,
+            ChunkHolder.PlayerProvider playersWatchingChunkProvider,
+            CallbackInfo ci
+    ) {
         this.changedBlocksPerSection = new ConcurrentShortHashSet[world.getSectionsCount()];
     }
 
-    @Redirect(method = "blockChanged", at = @At(value = "FIELD", target = "Lnet/minecraft/server/level/ChunkHolder;changedBlocksPerSection:[Lit/unimi/dsi/fastutil/shorts/ShortSet;", args = "array=set"))
-    private void setBlockChanged(ShortSet[] array, int index, ShortSet value) {
-        array[index] = new ConcurrentShortHashSet();
+    @Inject(method = "blockChanged", at = @At("HEAD"), cancellable = true)
+    private void onBlockChanged(BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+        if (this.getTickingChunk() == null) {
+            cir.setReturnValue(false);
+            return;
+        }
+
+        int index = this.levelHeightAccessor.getSectionIndex(pos.getY());
+        ShortSet set = this.changedBlocksPerSection[index];
+
+        if (set == null) {
+            synchronized (this.async$lock) {
+                set = this.changedBlocksPerSection[index];
+                if (set == null) {
+                    set = new ConcurrentShortHashSet();
+                    this.changedBlocksPerSection[index] = set;
+                    this.hasChangedSections = true;
+                }
+            }
+        }
+
+        set.add(SectionPos.sectionRelativePos(pos));
+        cir.setReturnValue(true);
     }
 }
