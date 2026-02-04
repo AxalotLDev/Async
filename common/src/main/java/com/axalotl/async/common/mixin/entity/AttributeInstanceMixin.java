e7606bad493d1a42b38d34180af5b98d9675a2e2
AxalotLDev <ladyginnic815@gmail.com>
2026-02-04 17:44:45 +0500

    fix AttributeInstance

--- common/src/main/java/com/axalotl/async/common/mixin/entity/AttributeInstanceMixin.java
+++ common/src/main/java/com/axalotl/async/common/mixin/entity/AttributeInstanceMixin.java
@@ -1,6 +1,8 @@
 package com.axalotl.async.common.mixin.entity;
 
 import com.axalotl.async.common.parallelised.ConcurrentCollections;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
@@ -20,4 +22,9 @@ public class AttributeInstanceMixin {
 
     @Shadow
     private final Map<Identifier, AttributeModifier> permanentModifiers = ConcurrentCollections.newHashMap();
+
+    @WrapMethod(method = "getModifiers(Lnet/minecraft/world/entity/ai/attributes/AttributeModifier$Operation;)Ljava/util/Map;")
+    private Map<Identifier, AttributeModifier> getModifiersConcurrent(AttributeModifier.Operation operation, Operation<Map<Identifier, AttributeModifier>> original) {
+        return modifiersByOperation.computeIfAbsent(operation, op -> ConcurrentCollections.newHashMap());
+    }
 }
