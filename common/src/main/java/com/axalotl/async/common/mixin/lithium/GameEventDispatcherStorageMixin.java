fe74d9db4824c86c0d8a019f89ac466f78f25f9c
furrymileon <gsafonov76@gmail.com>
2026-02-13 02:01:07 +0300

    Fixed NullPointerException in GameEventDispatcherStorage

---
+++ common/src/main/java/com/axalotl/async/common/mixin/lithium/GameEventDispatcherStorageMixin.java
@@ -0,0 +1,44 @@
+package com.axalotl.async.common.mixin.lithium;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import net.caffeinemc.mods.lithium.common.world.GameEventDispatcherStorage;
+import net.minecraft.world.level.gameevent.GameEventListenerRegistry;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+@Mixin(value = GameEventDispatcherStorage.class, remap = false)
+public class GameEventDispatcherStorageMixin {
+
+    @Unique
+    private final Object async$lock = new Object();
+
+    @WrapMethod(method = "addChunk")
+    private void async$addChunk(long pos, Int2ObjectMap<GameEventListenerRegistry> dispatchers, Operation<Void> original) {
+        synchronized (async$lock) {
+            original.call(pos, dispatchers);
+        }
+    }
+
+    @WrapMethod(method = "removeChunk")
+    private void async$removeChunk(long pos, Operation<Void> original) {
+        synchronized (async$lock) {
+            original.call(pos);
+        }
+    }
+
+    @WrapMethod(method = "replace")
+    private void async$replace(long pos, Int2ObjectMap<GameEventListenerRegistry> dispatchers, Operation<Void> original) {
+        synchronized (async$lock) {
+            original.call(pos, dispatchers);
+        }
+    }
+
+    @WrapMethod(method = "get")
+    private Int2ObjectMap<GameEventListenerRegistry> async$get(long pos, Operation<Int2ObjectMap<GameEventListenerRegistry>> original) {
+        synchronized (async$lock) {
+            return original.call(pos);
+        }
+    }
+}
