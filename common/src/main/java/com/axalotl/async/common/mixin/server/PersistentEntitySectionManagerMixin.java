145503eca5bf213bdd1037331bece7b45b15b7ec
furrymileon <gsafonov76@gmail.com>
2026-02-15 14:31:20 +0300

    optimization

--- common/src/main/java/com/axalotl/async/common/mixin/server/PersistentEntitySectionManagerMixin.java
+++ common/src/main/java/com/axalotl/async/common/mixin/server/PersistentEntitySectionManagerMixin.java
@@ -1,15 +1,20 @@
 package com.axalotl.async.common.mixin.server;
 
+import com.axalotl.async.common.parallelised.fastutil.Long2ObjectConcurrentHashMap;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import net.minecraft.world.level.ChunkPos;
+import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
 import net.minecraft.world.level.entity.EntityAccess;
 import net.minecraft.world.level.entity.PersistentEntitySectionManager;
 import net.minecraft.world.level.entity.Visibility;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(PersistentEntitySectionManager.class)
 public abstract class PersistentEntitySectionManagerMixin implements AutoCloseable {
+
+    @Shadow
+    private Long2ObjectMap<Visibility> chunkVisibility = new Long2ObjectConcurrentHashMap<>();
 
     @WrapMethod(method = "getEffectiveStatus")
     private static <T extends EntityAccess> Visibility getEffectiveStatus(T entity, Visibility visibility, Operation<Visibility> original) {
@@ -18,12 +23,5 @@ public abstract class PersistentEntitySectionManagerMixin implements AutoCloseab
             return entity.isAlwaysTicking() ? Visibility.TICKING : Visibility.TRACKED;
         }
         return result;
-    }
-
-    @WrapMethod(method = "canPositionTick(Lnet/minecraft/world/level/ChunkPos;)Z")
-    private boolean async$canPositionTick(ChunkPos pos, Operation<Boolean> original) {
-        synchronized (this) {
-            return original.call(pos);
-        }
     }
 }
