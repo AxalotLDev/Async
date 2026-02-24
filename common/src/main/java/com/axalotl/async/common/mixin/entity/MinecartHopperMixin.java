0bc5172c8142c8ae27a0d654af789ea4a6bfa169
AxalotLDev <ladyginnic815@gmail.com>
2026-02-24 21:20:37 +0500

    Test fix - test fix PersistentEntitySectionManager - test async MinecartHopperMixin (disabled now)

---
+++ common/src/main/java/com/axalotl/async/common/mixin/entity/MinecartHopperMixin.java
@@ -0,0 +1,20 @@
+package com.axalotl.async.common.mixin.entity;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import net.minecraft.world.entity.vehicle.minecart.MinecartHopper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+@Mixin(MinecartHopper.class)
+public class MinecartHopperMixin {
+    @Unique
+    private static final Object async$lock = new Object();
+
+    @WrapMethod(method = "suckInItems")
+    private boolean suckInItems(Operation<Boolean> original) {
+        synchronized (async$lock) {
+            return original.call();
+        }
+    }
+}
--- common/src/main/java/com/axalotl/async/common/mixin/server/PersistentEntitySectionManagerCallbackMixin.java
+++ common/src/main/java/com/axalotl/async/common/mixin/server/PersistentEntitySectionManagerCallbackMixin.java
@@ -1,29 +1,25 @@
 package com.axalotl.async.common.mixin.server;
 
-import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.entity.PersistentEntitySectionManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(PersistentEntitySectionManager.Callback.class)
 public abstract class PersistentEntitySectionManagerCallbackMixin implements AutoCloseable {
 
-    @Unique
-    private static final Object async$lock = new Object();
-
-    @WrapMethod(method = "onMove")
-    private void onMove(Operation<Void> original) {
-        synchronized (async$lock) {
-            original.call();
-        }
-    }
-
-    @WrapMethod(method = "onRemove")
-    private void onRemove(Entity.RemovalReason reason, Operation<Void> original) {
-        synchronized (async$lock) {
-            original.call(reason);
-        }
-    }
+//    @Unique
+//    private static final Object async$lock = new Object();
+//
+//    @WrapMethod(method = "onMove")
+//    private void onMove(Operation<Void> original) {
+//        synchronized (async$lock) {
+//            original.call();
+//        }
+//    }
+//
+//    @WrapMethod(method = "onRemove")
+//    private void onRemove(Entity.RemovalReason reason, Operation<Void> original) {
+//        synchronized (async$lock) {
+//            original.call(reason);
+//        }
+//    }
 }
