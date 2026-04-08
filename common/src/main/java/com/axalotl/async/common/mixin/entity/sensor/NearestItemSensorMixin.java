3f4ff77027cfedf016541f2efc952a9fcb945adc
AxalotLDev <ladyginnic815@gmail.com>
2026-04-08 22:29:54 +0500

    Revert "fix bugs with sensors"
    This reverts commit 7fdf952a36a49635ab56a38c63a6432e6ab6a985.

---
+++ common/src/main/java/com/axalotl/async/common/mixin/entity/sensor/NearestItemSensorMixin.java
@@ -0,0 +1,43 @@
+package com.axalotl.async.common.mixin.entity.sensor;
+
+import com.axalotl.async.common.utils.FastBitRadixSort;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.Brain;
+import net.minecraft.world.entity.ai.memory.MemoryModuleType;
+import net.minecraft.world.entity.ai.sensing.NearestItemSensor;
+import net.minecraft.world.entity.item.ItemEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+import java.util.*;
+
+@Mixin(value = NearestItemSensor.class, priority = 1500)
+public class NearestItemSensorMixin {
+    @Unique
+    private static final FastBitRadixSort itemSorter = new FastBitRadixSort();
+
+    @WrapMethod(method = "doTick(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/entity/Mob;)V")
+    private void doTick(ServerLevel level, Mob body, Operation<Void> original) {
+        Brain<?> brain = body.getBrain();
+        List<ItemEntity> list = level.getEntitiesOfClass(
+                ItemEntity.class,
+                body.getBoundingBox().inflate(32.0F, 16.0F, 32.0F),
+                _ -> true
+        );
+
+        Object[] arr = list.toArray();
+        itemSorter.sort(arr, arr.length, body.position());
+
+        Optional<ItemEntity> optional = Arrays.stream(arr)
+                .map(o -> (ItemEntity) o)
+                .filter(e -> body.wantsToPickUp(level, e.getItem()))
+                .filter(e -> e.closerThan(body, 32.0F))
+                .filter(body::hasLineOfSight)
+                .findFirst();
+
+        brain.setMemory(MemoryModuleType.NEAREST_VISIBLE_WANTED_ITEM, optional);
+    }
+}
