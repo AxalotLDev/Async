e4f724c9f9d8ba46cbbd70f86a6dc0dfd790e021
AxalotLDev <ladyginnic815@gmail.com>
2026-06-21 11:25:28 +0500

    SulfurCube pickUpItem wrap

---
+++ common/src/main/java/com/axalotl/async/common/mixin/entity/SulfurCubeMixin.java
@@ -0,0 +1,18 @@
+package com.axalotl.async.common.mixin.entity;
+
+import com.axalotl.async.api.utils.SyncItemPickup;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.monster.cubemob.SulfurCube;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(SulfurCube.class)
+public class SulfurCubeMixin {
+
+    @WrapMethod(method = "pickUpItem")
+    private void pickUpItem(ServerLevel level, ItemEntity entity, Operation<Void> original) {
+        SyncItemPickup.wrap(level, entity, () -> original.call(level, entity));
+    }
+}
