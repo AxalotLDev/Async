0b4d820657015c5fed6da9302541a5203538f548
AxalotLDev <ladyginnic815@gmail.com>
2026-06-26 10:55:51 +0500

    fix AttributeInstance

--- common/src/main/java/com/axalotl/async/common/mixin/entity/AttributeInstanceMixin.java
+++ common/src/main/java/com/axalotl/async/common/mixin/entity/AttributeInstanceMixin.java
@@ -27,4 +27,10 @@ public class AttributeInstanceMixin {
     private Map<ResourceLocation, AttributeModifier> getModifiersConcurrent(AttributeModifier.Operation operation, Operation<Map<ResourceLocation, AttributeModifier>> original) {
         return modifiersByOperation.computeIfAbsent(operation, ignored -> ConcurrentCollections.newHashMap());
     }
+
+    @WrapMethod(method = "addModifier")
+    private void addModifierIdempotent(AttributeModifier modifier, Operation<Void> original) {
+        modifierById.remove(modifier.id());
+        original.call(modifier);
+    }
 }
