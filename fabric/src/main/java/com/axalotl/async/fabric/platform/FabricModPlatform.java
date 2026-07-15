31a2f6331da9bbcc56d93da247043eedbe49ea19
alchemyyy <github@grassmail.net>
2026-07-15 10:24:18 -0700

    fix fabric common refmap loading

--- fabric/src/main/java/com/axalotl/async/fabric/platform/FabricModPlatform.java
+++ fabric/src/main/java/com/axalotl/async/fabric/platform/FabricModPlatform.java
@@ -20,4 +20,9 @@ public class FabricModPlatform implements ModPlatform {
     public boolean isModLoaded(String id) {
         return FabricLoader.getInstance().isModLoaded(id);
     }
+
+    @Override
+    public boolean platformUsesRefmap() {
+        return true;
+    }
 }
