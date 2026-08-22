b7949325500b106e6e5d69f2b5469043bb9df977
AxalotLDev <ladyginnic815@gmail.com>
2026-08-22 11:03:59 +0500

    Fix lock-order-inversion deadlock in Lithium block-change tracker mixins (#195)

---
+++ common/src/main/java/com/axalotl/async/common/mixin/lithium/LithiumBlockChangeTrackerLock.java
@@ -0,0 +1,8 @@
+package com.axalotl.async.common.mixin.lithium;
+
+public final class LithiumBlockChangeTrackerLock {
+    public static final Object LOCK = new Object();
+
+    private LithiumBlockChangeTrackerLock() {
+    }
+}
